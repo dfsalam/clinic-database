352971c2ac143fe09ba8d7a264b2eeec10aa5ca7
Luis Pomare <luispomare92@gmail.com>
2023-04-04 12:44:08 -0500

    Add create database statement

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,14 +1,18 @@
+CREATE DATABASE clinic;
+
 CREATE TABLE patients (
   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(255),
   date_of_birth DATE
 );
+
 CREATE TABLE medical_histories (
   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   admitted_at TIMESTAMP,
   patient_id INTEGER ,
   status VARCHAR(255)  
 );
+
 ALTER TABLE medical_histories
 ADD CONSTRAINT fk_patients
   FOREIGN KEY (patient_id)
@@ -20,7 +24,9 @@ CREATE TABLE invoices (
   payed_at TIMESTAMP,
   medical_history_id INTEGER  
 );
+
 ALTER TABLE invoices
 ADD CONSTRAINT fk_medical_histories
   FOREIGN KEY (medical_history_id)
   REFERENCES medical_histories(id);
+  
